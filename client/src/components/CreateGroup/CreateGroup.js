2fd1702e680343b4565a9f99ee23ea1c89b6df29
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-11-05 13:15:42 -0600

    cleaned up join/create modal

--- client/src/components/CreateGroup/CreateGroup.js
+++ client/src/components/CreateGroup/CreateGroup.js
@@ -1,15 +1,19 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import { CREATE_GROUP } from "../../utils/mutations";
 import { useMutation } from "@apollo/client";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
+
 const CreateGroup = (props) => {
   const [createGroup] = useMutation(CREATE_GROUP);
   const [groupName, setGroupName] = useState("");
+  const [groupPassword, setGroupPassword] = useState("");
   const handleFormSubmit = async (event) => {
     try {
       const { data } = await createGroup({
-        variables: { groupName},
+        variables: { groupName },
       });
     } catch (e) {
       console.error(e);
@@ -18,35 +22,58 @@ const CreateGroup = (props) => {
   };
 
   const handleInputChange = (e) => {
-    const {target} = e;
-    setGroupName(target.value);
-
-  }
-
+    const { target } = e;
+    if (target.name === "groupName") {
+      setGroupName(target.value);
+    }
+    if (target.name === "password") {
+      setGroupPassword(target.value);
+    }
+  };
 
   return (
-    <div className="bg-white">
-      <h1> Create Group</h1>
-      <TextField
-        name="groupName"
-        required
-        fullWidth
-        id="groupName"
-        label="groupName"
-        value={groupName}
-        onChange={handleInputChange}
-
-        autoFocus
-      />
-      <Button
-        type="submit"
-        fullWidth
-        variant="contained"
-        onClick={handleFormSubmit}
-        sx={{ mt: 3, mb: 2 }}
-      >
-        Sign In
-      </Button>
+    <div>
+      <Container component="main" maxWidth="xs" className="bg-slate-100">
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <h1> Create Group</h1>
+          <TextField
+            name="groupName"
+            required
+            fullWidth
+            id="groupName"
+            label="groupName"
+            value={groupName}
+            onChange={handleInputChange}
+            autoFocus
+          />
+          <TextField
+            name="password"
+            required
+            fullWidth
+            id="password"
+            label="password"
+            value={groupPassword}
+            onChange={handleInputChange}
+            autoFocus
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            onClick={handleFormSubmit}
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+        </Box>
+      </Container>
     </div>
   );
 };
--- client/src/components/JoinGroup/JoinGroup.js
+++ client/src/components/JoinGroup/JoinGroup.js
@@ -1,18 +1,20 @@
-import React, { useState} from "react";
+import React, { useState } from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import { CREATE_GROUP } from "../../utils/mutations";
 import { useMutation } from "@apollo/client";
+import Container from "@mui/material/Container";
+import Box from "@mui/material/Box";
 
 const JoinGroup = (props) => {
   const [joinGroup] = useMutation(CREATE_GROUP);
   const [groupName, setGroupName] = useState("");
   const [groupPassword, setGroupPassword] = useState("");
-  
+
   const handleFormSubmit = async (event) => {
     try {
       const { data } = await joinGroup({
-        variables: { groupName},
+        variables: { groupName },
       });
     } catch (e) {
       console.error(e);
@@ -21,49 +23,58 @@ const JoinGroup = (props) => {
   };
 
   const handleInputChange = (e) => {
-    const {target} = e;
-    if(target.name === 'groupName'){
+    const { target } = e;
+    if (target.name === "groupName") {
       setGroupName(target.value);
     }
-    if(target.name === "password"){
+    if (target.name === "password") {
       setGroupPassword(target.value);
     }
-    
-  }
-
+  };
 
   return (
-    <div className="bg-white">
-      <h1> Join Group</h1>
-      <TextField
-        name="groupName"
-        required
-        fullWidth
-        id="groupName"
-        label="groupName"
-        value={groupName}
-        onChange={handleInputChange}
-        autoFocus
-      />
-      <TextField
-        name="password"
-        required
-        fullWidth
-        id="password"
-        label="password"
-        value={groupPassword}
-        onChange={handleInputChange}
-        autoFocus
-      />
-      <Button
-        type="submit"
-        fullWidth
-        variant="contained"
-        onClick={handleFormSubmit}
-        sx={{ mt: 3, mb: 2 }}
-      >
-        Sign In
-      </Button>
+    <div>
+      <Container component="main" maxWidth="xs" className="bg-slate-100">
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <h1> Join Group</h1>
+          <TextField
+            name="groupName"
+            required
+            fullWidth
+            id="groupName"
+            label="groupName"
+            value={groupName}
+            onChange={handleInputChange}
+            autoFocus
+          />
+          <TextField
+            name="password"
+            required
+            fullWidth
+            id="password"
+            label="password"
+            value={groupPassword}
+            onChange={handleInputChange}
+            autoFocus
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            onClick={handleFormSubmit}
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+        </Box>
+      </Container>
     </div>
   );
 };
