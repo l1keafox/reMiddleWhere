67f885fbf2be87eed7a9cf78868d8b038e459a39
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-11-05 12:52:11 -0600

    changed Pets seed into no password

--- client/src/components/JoinGroup/JoinGroup.js
+++ client/src/components/JoinGroup/JoinGroup.js
@@ -1,15 +1,55 @@
-import React, { Component } from 'react'
+import React, { useState} from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { CREATE_GROUP } from "../../utils/mutations";
+import { useMutation } from "@apollo/client";
+const JoinGroup = (props) => {
+  const [joinGroup] = useMutation(CREATE_GROUP);
+  const [groupName, setGroupName] = useState("");
+  const handleFormSubmit = async (event) => {
+    try {
+      const { data } = await joinGroup({
+        variables: { groupName},
+      });
+    } catch (e) {
+      console.error(e);
+    }
+    props.doClose();
+  };
 
-export default class JoinGroup extends Component {
-  render() {
-    return (
-      <div>
-        JoinGroup
-        JoinGroup
-        JoinGroup
-        
-      </div>
+  const handleInputChange = (e) => {
+    const {target} = e;
+    setGroupName(target.value);
 
-    )
   }
-}
+
+
+  return (
+    <div className="bg-white">
+      <h1> Join Group</h1>
+      <TextField
+        autoComplete="given-name"
+        name="userName"
+        required
+        fullWidth
+        id="userName"
+        label="userName"
+        value={groupName}
+        onChange={handleInputChange}
+
+        autoFocus
+      />
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        onClick={handleFormSubmit}
+        sx={{ mt: 3, mb: 2 }}
+      >
+        Sign In
+      </Button>
+    </div>
+  );
+};
+
+export default JoinGroup;
