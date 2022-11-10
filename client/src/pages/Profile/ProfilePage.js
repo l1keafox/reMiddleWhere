5f6d2e62930e3e9cd1e47bb7d9273522a99990ad
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-11-10 10:26:51 -0700

    Cleaned up

--- client/src/pages/Profile/ProfilePage.js
+++ client/src/pages/Profile/ProfilePage.js
@@ -4,25 +4,20 @@ import { useExistingUserContext } from "../../utils/existingUserContext";
 import auth from "../../utils/auth";
 import { QUERY_ME } from "./../../utils/queries";
 import { useQuery } from "@apollo/client";
-import Paper from '@mui/material/Paper';
+import Paper from "@mui/material/Paper";
 import { Button } from "@mui/material";
 function ProfilePage(props) {
   const { existingUser } = useExistingUserContext();
-  const { loading, data,startPolling,stopPolling } = useQuery(QUERY_ME,{
-    pollInterval: 500
+  const { loading, data, startPolling, stopPolling } = useQuery(QUERY_ME, {
+    pollInterval: 500,
   });
-  console.log( "PROFILE PAGE", auth.getUser().data, data,loading);
-  
+  console.log("PROFILE PAGE", auth.getUser().data, data, loading);
+
   useEffect(() => {
     startPolling(500);
-    console.log('start polling?');
   }, []);
-  useEffect( () => () => stopPolling(), [] );
-  useEffect(() => {
-    if(data)
-    console.log('data change',data.me.groups.length );
-  }, [data]);
-  
+  useEffect(() => () => stopPolling(), []);
+
   return (
     <div>
       {loading ? (
@@ -33,24 +28,23 @@ function ProfilePage(props) {
           <h1> WHO AM I: {auth.getUser().data.username}</h1>
           <h1> my id: {auth.getUser().data._id}</h1>
           <h1> my email: {auth.getUser().data.email}</h1>
-          <div className = "flex justify-center"> 
-          {
-            data.me.groups.map((group,index) => (
-              <Paper className = "w-1/4 p-3 m-3 hover:bg-slate-200" key={index}  > 
+          <div className="flex justify-center">
+            {data.me.groups.map((group, index) => (
+              <Paper className="w-1/4 p-3 m-3 hover:bg-slate-200" key={index}>
                 <h1> Group Name: {group.name} </h1>
                 <h1> Group id: {group._id} </h1>
                 <h1> Users : </h1>
-                    {group.users.map((user,index2)=>(
-                      <h2 key={index2} > user </h2>
-                    ))
-                    }
+                {group.users.map((user, index2) => (
+                  <h2 key={index2}> user </h2>
+                ))}
                 <h2> Lat :</h2>
                 <h2> Long :</h2>
-                <Button data-id ={group._id} onClick={props.mapSelect}> Click Here To Go To Map</Button>
+                <Button data-id={group._id} onClick={props.mapSelect}>
+                  {" "}
+                  Click Here To Go To Map
+                </Button>
               </Paper>
-            )
-            )
-          }
+            ))}
           </div>
         </div>
       )}
