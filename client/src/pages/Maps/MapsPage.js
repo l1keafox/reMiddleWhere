b9a9aebfedc24dbec3f9a2c730c27d87eb294c47
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-11-28 17:49:20 -0700

    cleaned up client

--- client/src/pages/Maps/MapsPage.js
+++ client/src/pages/Maps/MapsPage.js
@@ -54,23 +54,20 @@ const MapsPage = function (props) {
   }, [data]);
   
   function upDatePos() {
-    console.log("doing update");
     navigator.geolocation.getCurrentPosition(async (position) => {
       //mutation AddUserLocationToGroup($userId: ID!, $groupId: ID!, $latitude: Int!, $longitude: Int!)
       // So we need 4 times, userID, groupID, lat, and long, here we get lat/long
       const userId = auth.getUser().data._id;
       const latitude = position.coords.latitude;
       const longitude = position.coords.longitude;
-      console.log(userId, groupId, latitude, longitude);
       const { data } = await addUserLocationToGroup({
         variables: { userId, groupId, latitude, longitude },
       });
       console.log("THIS:",data);
       if(data){
         alert('Location Updated');
-        refetch({ variables: { groupId } });
+        refetch({  groupId } );
       }
-      console.log(error);
     });
   }
 
