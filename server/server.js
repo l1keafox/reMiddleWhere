fe362205a70f7772ae45a583d3fb20a81d0dd1cc
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-12-02 09:36:20 -0700

    cosmetic

--- server/server.js
+++ server/server.js
@@ -30,6 +30,7 @@ app.use((req, res, next) => {
   res.append('Access-Control-Allow-Origin', ['*']);
   res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.append('Access-Control-Allow-Headers', 'Content-Type');
+  
   next();
 });
 app.get("/", (req, res) => {
