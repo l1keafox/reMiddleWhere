9b74668151591506f1d883883d30ecf11fdbac83
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-12-02 09:46:34 -0700

    something

--- server/server.js
+++ server/server.js
@@ -24,7 +24,6 @@ app.use((req, res, next) => {
   res.append('Access-Control-Allow-Origin', ['*']);
   res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.append('Access-Control-Allow-Headers', 'Content-Type');
-  
   next();
 });
 app.use(express.urlencoded({ extended: false }));
