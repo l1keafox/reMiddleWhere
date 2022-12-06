1040ee9ffe43f4a66f80fab7f62e1355a88fa266
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-12-05 19:55:32 -0700

    mediaq done

--- client/src/pages/Landing/LandingPage.js
+++ client/src/pages/Landing/LandingPage.js
@@ -28,13 +28,13 @@ function LandingPage(props) {
     <div  className="flex flex-col h-screen justify-center" >
       <div className="flex container flex-col lg:flex-row">
         <div className="bg-slate-200 lg:w-1/2 p-3 lg:h-[40rem] cursor-default select-none ">
-          <h1 className="text-7xl  font-marker text-center  "> Middle<span className="text-green-700">Where</span></h1>
+          <h1 className="text-5xl lg:text-7xl  font-marker text-center  "> Middle<span className="text-green-700">Where</span></h1>
           <br />
-          <p className="font-noto text-yellow-800 text-2xl p-3 text-justify cursor-default select-none">
+          <p className="font-noto text-yellow-800 xl:text-2xl p-3 text-justify cursor-default select-none">
             Middle-Where is a web-based application designed to bring friends
             together using a calculated, centralized geolocation.
             </p>
-            <p className="font-noto text-yellow-800 text-2xl p-3 text-justify  cursor-default select-none"> The motivation
+            <p className="font-noto text-yellow-800 xl:text-2xl p-3 text-justify  cursor-default select-none"> The motivation
             behind this project is to streamline the process of deciding the
             best place to meet between multiple group members. Individuals can
             choose to be placed into groups with their friends or colleagues,
--- client/src/pages/Profile/InfoCard.js
+++ client/src/pages/Profile/InfoCard.js
@@ -3,9 +3,9 @@ import auth from '../../utils/auth'
 import avatar from './placeAvatar.PNG'
 const InfoCard = () => {
   return (
-    <div className='border border-slate-100 bg-slate-800 text-white opacity-95 flex justify-start mx-20 '>
+    <div className='border border-slate-100 bg-slate-800 text-white opacity-95 flex flex-col sm:flex-row  justify-start mx-20 '>
           <img src={avatar} className="flex-none w-1/7 rounded-full opacity-100 p-2"></img>
-          <div className='pl-4'>
+          <div className='sm:pl-4'>
             <h1 className='text-5xl font-bold cursor-default select-none'> {auth.getUser().data.username} <span className='text-sm font-medium pl-5 cursor-default select-text'> {auth.getUser().data._id}</span></h1>
             <h1 className='font-medium text-xl cursor-default select-none'>{auth.getUser().data.email}</h1>
           </div>
