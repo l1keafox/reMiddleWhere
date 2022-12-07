8740839ee8996dd20545ed565707ccf1d04d85d1
Raymond Lewis <raymond.ed.lewis@gmail.com>
2022-12-06 17:48:47 -0700

    Rename chat to instant Mesaging

--- client/src/pages/Maps/ChatWindow.js
+++ client/src/pages/Maps/ChatWindow.js
@@ -43,9 +43,9 @@ function ChatWindow(props) {
 
   return (
     <div>
-      <h1 className="font-bold items-center justify-center flex text-xl">ChatWindow</h1>
+      <h1 className="font-bold items-center justify-center flex text-xl">Instant Messaging</h1>
       <form onSubmit={doThis} className="flex">
-        <TextField value={name} onChange={handleInputChange}  variant="standard" className="w-3/4  m-2 p-1 "/>
+        <TextField value={name} onChange={handleInputChange} label="Send a message" variant="standard" className="w-3/4  m-2 p-1 "/>
         <button  className="bg-yellow-300 m-2 p-1 px-3 border-2 border-blue-200 hover:bg-yellow-400" > SEND </button>
       </form>
       <div style={{overflowX:"scroll",overflowY:"scroll", height:"400px"}}>
